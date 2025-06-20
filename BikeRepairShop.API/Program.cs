bafcb2ab0abbf147ef25c3a924d4e78e2486f50f
Nigussie Girma <girma@utviklerne.no>
2025-06-20 12:38:26 +0200

    change booking to minimal api format instead of traditional control

--- BikeRepairShop.API/Program.cs
+++ BikeRepairShop.API/Program.cs
@@ -1,6 +1,7 @@
 using BikeRepairShop.API.Contexts;
 using BikeRepairShop.API.Data;
 using BikeRepairShop.API.Helpers;
+using BikeRepairShop.API.Models.Dtos;
 using BikeRepairShop.API.Services;
 using Microsoft.EntityFrameworkCore;
 
@@ -41,6 +42,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 if (builder.Environment.IsEnvironment("Docker"))
 {
@@ -68,4 +70,92 @@ app.UseSwaggerUI();
 app.MapControllers();
 app.UseRouting();
 app.UseCors("AllowFrontend");
+
+// Usage of minimal api for booking instead of controller :) 
+
+var bookings = app.MapGroup("/api/bookings").WithTags("bookings");
+
+bookings.MapGet("/{id:int}", async (int id, IBookingService bookingService, ILoggerFactory loggerFactory) =>
+{
+    var loggger = loggerFactory.CreateLogger(nameof(bookingService));
+    try
+    {
+        loggger.LogInformation("getting booking");
+        var booking = await bookingService.GetBookingByIdAsync(id);
+
+        return Results.Ok(booking);
+    }
+    catch(Exception ex)
+    {
+        loggger.LogError(ex.Message);
+        return Results.NotFound();
+    }
+});
+
+bookings.MapGet("/", async (IBookingService bookingService, ILoggerFactory loggerFactory) =>
+{
+    var logger = loggerFactory.CreateLogger(nameof(bookingService));
+    try
+    {
+        logger.LogInformation("getting all booking");
+        var bookings = await bookingService.GetAllBookingsAsync();
+        return Results.Ok(bookings);    
+    }
+    catch(Exception ex)
+    {
+        logger.LogError(ex.Message);
+        return Results.NotFound(ex.Message);
+    }
+});
+
+bookings.MapPost("/", async (BookingCreateDto bookingCreateDto,IBookingService bookingService, ILoggerFactory loggerFactory) =>
+{
+    var logger = loggerFactory.CreateLogger(nameof(BookingService));
+
+    try
+    {
+        logger.LogInformation("adding a booking");
+        await bookingService.AddBookingAsync(bookingCreateDto);
+        return Results.Ok();
+    }
+    catch(Exception ex)
+    {
+        logger.LogError(ex.Message);
+        return Results.BadRequest();
+    }
+});
+
+app.MapPut("/", async (BookingDto bookingDto, IBookingService bookingService, ILoggerFactory loggerFactory) =>
+{
+    var logger = loggerFactory.CreateLogger(typeof(BookingService));
+    try
+    {
+        logger.LogInformation("updating booking");
+
+        await bookingService.UpdateBookingAsync(bookingDto);
+        return Results.Ok();
+    }
+    catch(Exception ex)
+    {
+        logger.LogError(ex.Message);
+        return Results.BadRequest(ex.Message);
+    }
+});
+
+app.MapDelete("/{id:int}", async (int id, IBookingService bookingService, ILoggerFactory loggerFactory) =>
+{
+    var logger = loggerFactory.CreateLogger("nameof(booking)");
+    try
+    {
+        logger.LogInformation("Deleting bookings");
+        await bookingService.DeleteBookingAsync(id);
+        return Results.Ok($"Booking with id {id} deleted");
+    }
+    catch (Exception ex)
+    {
+        logger.LogError(ex.Message);
+        return Results.BadRequest(ex.Message);
+    }
+});
+
 app.Run();
